d4456aab558c95793c1537c978f8a4ea26d548c7
Ajaya Khatiwada <khatiwadaajaya@gmail.com>
2019-01-30 11:20:34 +1100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleNavigation() {
+  let x = document.getElementById("myTopnav");
+  console.log(x);
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
